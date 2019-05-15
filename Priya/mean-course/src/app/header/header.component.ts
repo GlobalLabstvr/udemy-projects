078ba57e6d9c3f0c646fbf2bf99678d48853e805
GlobalLabstvr <GlobalLabsTvr@gmail.com>
2019-05-15 14:45:26 +0530

    optimizations working

---
+++ Priya/mean-course/src/app/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+selector:'app-header',
+templateUrl:'/header.component.html',
+styleUrls:["./header.component.css"]
+})
+
+
+export class HeaderComponent implements OnInit,OnDestroy{
+    userIsAuthenticated = false;
+    private authListenerSubs:Subscription;
+constructor(private authservice:AuthService){}
+ngOnInit(){
+    this.userIsAuthenticated = this.authservice.getIsAuth();
+    this.authListenerSubs = this.authservice.getAuthStatusListener()
+    .subscribe(isAuthenticated =>{
+this.userIsAuthenticated = isAuthenticated;
+    });
+}
+
+ngOnDestroy(){
+ this.authListenerSubs.unsubscribe();
+}
+onLogout(){
+    this.authservice.logout();
+}
+}
